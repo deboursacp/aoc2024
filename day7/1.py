3a7729abc3d8275aa5040711c6aa84f1636da602
deboursacp <deboursacp@gmail.com>
2024-12-06 21:26:40 -0800

    Day 7; Went for speed, took about 25 mins. Didn't try to optimize

---
+++ day7/1.py
@@ -0,0 +1,26 @@
+import itertools
+
+with open("in.txt") as f:
+    lines = f.readlines()
+
+
+def evaluate(operands: list[int], operations: list[str]) -> int:
+    # Can't use eval() since we always go left to right.
+    running_total = operands[0]
+    for i, operand in enumerate(operands[1:]):
+        if operations[i] == "*":
+            running_total *= operand
+        elif operations[i] == "+":
+            running_total += operand
+    return running_total
+
+
+tot = 0
+for l in lines:
+    tv, operands = l.split(":")
+    operands = list(map(int, operands.split()))
+    for operations in itertools.product(("*", "+"), repeat=len(operands) - 1):
+        if evaluate(operands, operations) == int(tv):
+            tot += int(tv)
+            break
+print(tot)
