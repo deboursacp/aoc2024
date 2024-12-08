48c095ed475884bbfb88a95ef7c6c477bb11dabc
deboursacp <deboursacp@gmail.com>
2024-12-08 00:29:40 -0800

    add day 8 tricks learned

---
+++ day8/tricks_learned.py
@@ -0,0 +1,4 @@
+# 1) Could have used permutation instead of combination to not need to traverse in both directions.
+# 2) should have still stuck with {(x,y): val} for my map and then used map.values() to iterate and list comprehension to get keys.
+# 3) set subtraction to ignore `.`
+# 4) for i in range(x), range(y) will iterate over the iterables. useful for part 2.
